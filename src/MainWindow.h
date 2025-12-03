6376697724c5e540edf058e19649aac1a7e38d5a
Thomas Lamy <thomas.lamy@netwake.de>
2025-12-03 07:55:17 +0100

    feat: set/reset current offset
    You can now set (and reset) a current offset by pressing the "d" key. This is to take out the additional power an adapter (like magsafe3 or usb-c to custom connector) may consume.

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -19,67 +19,93 @@ class QProgressBar;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  void tryConnectLastDevice();
-  explicit MainWindow(OsdSettings *settings, QWidget *parent = nullptr);
-  ~MainWindow() override;
-  void startReconnectTimer() const;
-  void showStatusMessage(const QString &message, int hideAfterMs);
+    void tryConnectLastDevice();
 
-  // Public getter for settings
-  [[nodiscard]] OsdSettings *getSettings() const { return settings; }
-  void onPrimaryFontChanged(const QFont &font);
-  void onSecondaryFontChanged(const QFont &font);
-  void onColorChanged();
+    explicit MainWindow(OsdSettings *settings, QWidget *parent = nullptr);
+
+    ~MainWindow() override;
+
+    void startReconnectTimer() const;
+
+    void showStatusMessage(const QString &message, int hideAfterMs);
+
+    // Public getter for settings
+    [[nodiscard]] OsdSettings *getSettings() const { return settings; }
+
+    void onPrimaryFontChanged(const QFont &font);
+
+    void onSecondaryFontChanged(const QFont &font);
+
+    void onColorChanged();
 
 private slots:
-  void onPowerDataReceived(const PowerData &data);
-  void onDeviceConnected(const QString &deviceName);
-  void onDeviceDisconnected();
-  void showSettings();
-  void updateLabels();
-  void hideStatusBar();
-  void connectLastDevice(bool reconnecting);
-  void toggleEnergy() const;
-  void showDeviceSelectionDialog();
-  void resetMeasurementHistory();
-  void showAboutDialog();
+    PowerData normalize(const PowerData &data);
 
+    void onPowerDataReceived(const PowerData &data);
 
+    void onDeviceConnected(const QString &deviceName);
+
+    void onDeviceDisconnected();
+
+    void showSettings();
+
+    void updateLabels();
+
+    void hideStatusBar();
+
+    void connectLastDevice(bool reconnecting);
+
+    void toggleEnergy() const;
+
+    void showDeviceSelectionDialog();
+
+    void resetMeasurementHistory();
+
+    void setBaseCurrent();
+
+    void resetBaseCurrent();
+
+    void showAboutDialog();
 
 protected:
-  void resizeEvent(QResizeEvent *event) override;
-  bool eventFilter(QObject *obj, QEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+    bool eventFilter(QObject *obj, QEvent *event) override;
 
 private:
-  void setupUI();
-  void updateUINoData();
-  void setBackgroundColor(const QColor &color);
-  void positionWidgets();
+    void setupUI();
 
-  PowerMonitor *m_powerMonitor;
-  DeviceManager *m_deviceManager;
-  SettingsDialog *m_settingsdialog;
-  OsdSettings *settings = nullptr;
-  MeasurementHistory *m_history = nullptr;
-  DeviceSelectionDialog *m_deviceSelectionDialog;
+    void updateUINoData();
 
-  QTimer *m_updateTimer;
-  QTimer *m_statusBarHideTimer;
+    void setBackgroundColor(const QColor &color);
 
-  // UI members
-  QLabel *lblVoltage;
-  QLabel *lblCurrent;
-  QLabel *lblPower;
-  QLabel *lblEnergy;
-  QLabel *lblMinMaxCurrent;
-  QFont fntPrimary;
-  QFont fntSecondary;
+    void positionWidgets();
 
-  CurrentGraph *m_currentGraph;
-  QTimer *m_reconnect_timer;
+    PowerMonitor *m_powerMonitor;
+    DeviceManager *m_deviceManager;
+    SettingsDialog *m_settingsdialog;
+    OsdSettings *settings = nullptr;
+    MeasurementHistory *m_history = nullptr;
+    DeviceSelectionDialog *m_deviceSelectionDialog;
+
+    QTimer *m_updateTimer;
+    QTimer *m_statusBarHideTimer;
+
+    // UI members
+    QLabel *lblVoltage;
+    QLabel *lblCurrent;
+    QLabel *lblPower;
+    QLabel *lblEnergy;
+    QLabel *lblMinMaxCurrent;
+    QFont fntPrimary;
+    QFont fntSecondary;
+
+    CurrentGraph *m_currentGraph;
+    QTimer *m_reconnect_timer;
+    PowerData lastDataRaw;
 };
 
 #endif // MAINWINDOW_H
